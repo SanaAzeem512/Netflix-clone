1187f2e11f7a6371150dfc5b889b60e44105dc21
SanaAzeem512 <sanaazeem416@gmail.com>
2024-08-25 14:23:53 +0530

    env setback

--- src/firebase.js
+++ src/firebase.js
@@ -13,13 +13,13 @@ import {
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
+    apiKey: "AIzaSyCVBJM87mMhfMgwSDCTxE3NCCXalPEj9wE",
+    authDomain: "netflix-clone-c2a88.firebaseapp.com",
+    projectId: "netflix-clone-c2a88",
+    storageBucket: "netflix-clone-c2a88.appspot.com",
+    messagingSenderId: "994494553006",
+    appId: "1:994494553006:web:ddef15631b20448a14463f"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
